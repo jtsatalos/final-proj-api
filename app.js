2cea2ef4e95253ecff9748e46bde6e1dfcf87f9e
Jorgos Tsatalos <jorgostsatalos@jorgoss-mbp.home>
2019-12-08 13:04:27 -0800

    change cors for surge

--- app.js
+++ app.js
@@ -7,9 +7,30 @@ const IS_DEVELOPMENT = ENVIRONMENT === "development";
 
 // middleware
 app.use(express.json());
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000"
+//   })
+// );
+
+var allowedOrigins = [
+  "http://localhost:3000",
+  "http://selective-burst.surge.sh/"
+];
 app.use(
   cors({
-    origin: "http://localhost:3000"
+    origin: function(origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
   })
 );
 
